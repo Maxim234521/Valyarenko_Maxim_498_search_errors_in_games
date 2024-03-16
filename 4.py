aa5b352ec0f3a79423b42ce6fe3492233c1f00c5
Maxim234521 <mvalyarenko2006@gmail.com>
2024-03-16 15:11:45 +0300

    обавил таблицу с колвом багов

--- 4.py
+++ 4.py
@@ -1,6 +1,7 @@
 import csv
 
 
+#ищем кол-во багов
 def search(name, data):
     k = 0
     for stroka in data:
@@ -8,11 +9,12 @@ def search(name, data):
             k += 1
             return k
 
-
+#открывем файл и создаем новый
 with open('game.csv', encoding='utf-8') as f, open('game_counter.csv', 'w', encoding='utf-8', newline='') as nf:
     data = list(csv.reader(f, delimiter=';'))
     res = csv.writer(nf, delimiter=';')
 
+    #добавляем новый столбец и записываем кол-во багов
     data[0].append('Errors count')
     res.writerow(data[0])
     for stroka in data[1:]:
